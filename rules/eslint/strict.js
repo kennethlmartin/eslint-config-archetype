536e3016e2192e08d733f498856b144774f9ab00
Kenneth Martin <kennethlmartin@gmail.com>
2018-11-05 01:24:34 -0500

    disallow strict mode directives

--- rules/eslint/strict.js
+++ rules/eslint/strict.js
@@ -9,6 +9,6 @@ module.exports = {
   'rules': {
     // Require or disallow strict mode directives
     // http://eslint.org/docs/rules/strict
-    'strict': ['error', 'global'],
+    'strict': ['error', 'never'],
   },
 };
