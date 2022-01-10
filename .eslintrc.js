0bedfb1f58e628c8d2232b203d97bc027b270555
Kenneth Martin <558152+kennethlmartin@users.noreply.github.com>
2022-01-10 02:24:13 -0500

    feat: update to ECMAScript 2021 env

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    es2020: true,
+    es2021: true,
     node: true,
   },
   extends: [
--- configurations/base.js
+++ configurations/base.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    es2020: true,
+    es2021: true,
     jest: true,
   },
   extends: [
